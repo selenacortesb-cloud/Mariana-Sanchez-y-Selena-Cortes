5a293404c61f7ab272c185632833e089192ed7f1
marianasanchezb1-create <mariana.sanchezb1@udea.edu.co>
2026-06-01 18:54:53 -0500

    Add clsPrestamo class for loan management

--- src/clsPrestamo.py
+++ src/clsPrestamo.py
@@ -1,1 +1,21 @@
+from datetime import datetime
 
+
+class clsPrestamo:
+
+    def __init__(
+        self,
+        documento_usuario,
+        codigo_item
+    ):
+
+        self.documento_usuario = documento_usuario
+        self.codigo_item = codigo_item
+        self.fecha_prestamo = datetime.now()
+
+    def mostrar_datos(self):
+
+        return (
+            f"{self.documento_usuario} - "
+            f"{self.codigo_item}"
+        )
