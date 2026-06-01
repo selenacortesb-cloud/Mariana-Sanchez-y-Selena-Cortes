885898903c7611712a0d1deaa621adcc6859e97d
marianasanchezb1-create <mariana.sanchezb1@udea.edu.co>
2026-06-01 18:51:07 -0500

    Refactor clsUsuarios to define a class structure

--- src/clsUsuarios.py
+++ src/clsUsuarios.py
@@ -1,7 +1,24 @@
-usuario1 = clsUsuarios(
-    "Mariana",
-    "Sanchez",
-    "1092730571",
-    "mariana.sanchezb1@udea.edu.co",
-    30
-)
+class clsUsuarios:
+
+    def __init__(
+        self,
+        nombre,
+        apellido,
+        documento,
+        correo,
+        tiempo_prestamo
+    ):
+
+        self.nombre = nombre
+        self.apellido = apellido
+        self.documento = documento
+        self.correo = correo
+        self.tiempo_prestamo = tiempo_prestamo
+
+    def mostrar_datos(self):
+
+        return (
+            f"{self.nombre} "
+            f"{self.apellido} "
+            f"- {self.documento}"
+        )
