df3ad78ea6073425dd9bce2ae11942a44c8eb267
marianasanchezb1-create <mariana.sanchezb1@udea.edu.co>
2026-06-01 18:57:45 -0500

    Implement main menu for user interactions

--- src/main.py
+++ src/main.py
@@ -1,1 +1,69 @@
+from validaciones import *
 
+def menu():
+
+    while True:
+
+        print("\n===== ALQUILOOP =====")
+
+        print("1. Registrar usuario")
+        print("2. Registrar item")
+        print("3. Registrar prestamo")
+        print("4. Registrar devolucion")
+        print("5. Consultar prestamos")
+        print("6. Administrador")
+        print("7. Salir")
+
+        opcion = input(
+            "\nSeleccione una opcion: "
+        )
+
+        if opcion == "1":
+
+            print(
+                "Modulo registro usuario"
+            )
+
+        elif opcion == "2":
+
+            print(
+                "Modulo registro item"
+            )
+
+        elif opcion == "3":
+
+            print(
+                "Modulo prestamos"
+            )
+
+        elif opcion == "4":
+
+            print(
+                "Modulo devoluciones"
+            )
+
+        elif opcion == "5":
+
+            print(
+                "Modulo consultas"
+            )
+
+        elif opcion == "6":
+
+            print(
+                "Modulo administrador"
+            )
+
+        elif opcion == "7":
+
+            print("Hasta luego")
+            break
+
+        else:
+
+            print(
+                "Opcion invalida"
+            )
+
+
+menu()
