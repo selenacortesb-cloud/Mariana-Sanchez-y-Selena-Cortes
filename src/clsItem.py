9552fdc18b293061d356e779d47ff8b01a2be5b0
marianasanchezb1-create <mariana.sanchezb1@udea.edu.co>
2026-06-01 18:51:28 -0500

    Add clsItem class with initialization and display method

--- src/clsItem.py
+++ src/clsItem.py
@@ -1,1 +1,23 @@
+class clsItem:
 
+    def __init__(
+        self,
+        codigo,
+        nombre,
+        categoria,
+        precio,
+        estado
+    ):
+
+        self.codigo = codigo
+        self.nombre = nombre
+        self.categoria = categoria
+        self.precio = precio
+        self.estado = estado
+
+    def mostrar_datos(self):
+
+        return (
+            f"{self.codigo} - "
+            f"{self.nombre}"
+        )
