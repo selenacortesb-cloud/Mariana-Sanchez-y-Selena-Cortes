3ca2af4f8747c3d1c42db4513bf27d38761610b0
marianasanchezb1-create <mariana.sanchezb1@udea.edu.co>
2026-06-01 18:56:53 -0500

    Add validation functions for name, document, email, and time

--- src/validaciones.py
+++ src/validaciones.py
@@ -1,1 +1,35 @@
+def validar_nombre(nombre):
 
+    return (
+        nombre.isalpha()
+        and len(nombre) >= 3
+    )
+
+
+def validar_apellido(apellido):
+
+    return (
+        apellido.isalpha()
+        and len(apellido) >= 3
+    )
+
+
+def validar_documento(documento):
+
+    return (
+        documento.isdigit()
+        and 3 <= len(documento) <= 15
+    )
+
+
+def validar_correo(correo):
+
+    return (
+        "@" in correo
+        and correo.endswith(".com")
+    )
+
+
+def validar_tiempo(tiempo):
+
+    return tiempo in [5, 10, 15, 30]
