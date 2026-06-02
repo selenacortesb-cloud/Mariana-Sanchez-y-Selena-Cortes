7aac3b90eace654c215970b553d912aa1e9851ea
marianasanchezb1-create <mariana.sanchezb1@udea.edu.co>
2026-06-01 20:52:37 -0500

    Fix file path for reading prestamos data

--- src/administrador.py
+++ src/administrador.py
@@ -1,17 +1,24 @@
 import pandas as pd
+
 def exportar_csv():
+
 ```
 datos = []
+
 try:
+
     with open(
-        "src/datos/prestamos.txt",
+        "src/data/prestamos.txt",
         "r",
         encoding="utf-8"
     ) as archivo:
+
         for linea in archivo:
+
             datos.append(
                 linea.strip().split(";")
             )
+
     df = pd.DataFrame(
         datos,
         columns=[
@@ -20,14 +27,18 @@ try:
             "Fecha"
         ]
     )
+
     df.to_csv(
         "src/exportaciones/prestamos.csv",
         index=False
     )
+
     print(
         "CSV generado correctamente"
     )
+
 except:
+
     print(
         "No existen prestamos"
     )
