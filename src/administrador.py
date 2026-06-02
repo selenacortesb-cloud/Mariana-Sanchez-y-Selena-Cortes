285beaf3c2f254fa264107e0657feafee622fa20
marianasanchezb1-create <mariana.sanchezb1@udea.edu.co>
2026-06-02 06:08:17 -0500

    Update administrador.py

--- src/administrador.py
+++ src/administrador.py
@@ -1,24 +1,16 @@
 import pandas as pd
-
-
 def exportar_csv():
-
     datos = []
-
     try:
-
         with open(
-            "src/datos/prestamos.txt",
+            "src/data/prestamos.txt",
             "r",
             encoding="utf-8"
         ) as archivo:
-
             for linea in archivo:
-
                 datos.append(
                     linea.strip().split(";")
                 )
-
         df = pd.DataFrame(
             datos,
             columns=[
@@ -27,18 +19,14 @@ def exportar_csv():
                 "Fecha"
             ]
         )
-
         df.to_csv(
             "src/exportaciones/prestamos.csv",
             index=False
         )
-
         print(
             "CSV generado correctamente"
         )
-
     except Exception as e:
-
         print(
             f"Error: {e}"
         )
