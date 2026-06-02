f3c8f7128236a4e6472fbc71ca49a4b8da9a7a1d
marianasanchezb1-create <mariana.sanchezb1@udea.edu.co>
2026-06-01 20:07:31 -0500

    Refactor export function to export CSV correctly

--- src/administrador.py
+++ src/administrador.py
@@ -1,44 +1,34 @@
 import pandas as pd
-
-
-def exportar_prestamos_csv():
-
-    datos = []
-
-    try:
-
-        with open(
-            "src/data/prestamos.txt",
-            "r",
-            encoding="utf-8"
-        ) as archivo:
-
-            for linea in archivo:
-
-                datos.append(
-                    linea.strip().split(";")
-                )
-
-        df = pd.DataFrame(
-            datos,
-            columns=[
-                "Documento",
-                "Item",
-                "Fecha"
-            ]
-        )
-
-        df.to_csv(
-            "src/exportaciones/prestamos.csv",
-            index=False
-        )
-
-        print(
-            "CSV exportado correctamente"
-        )
-
-    except:
-
-        print(
-            "No existen prestamos"
-        )
+def exportar_csv():
+```
+datos = []
+try:
+    with open(
+        "src/datos/prestamos.txt",
+        "r",
+        encoding="utf-8"
+    ) as archivo:
+        for linea in archivo:
+            datos.append(
+                linea.strip().split(";")
+            )
+    df = pd.DataFrame(
+        datos,
+        columns=[
+            "Documento",
+            "Item",
+            "Fecha"
+        ]
+    )
+    df.to_csv(
+        "src/exportaciones/prestamos.csv",
+        index=False
+    )
+    print(
+        "CSV generado correctamente"
+    )
+except:
+    print(
+        "No existen prestamos"
+    )
+```
